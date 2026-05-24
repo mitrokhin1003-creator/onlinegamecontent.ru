1ee370977100398a2b305c70fb03b2272ff31973
mitrokhin1003-creator <mitrokhin1003@gmail.com>
2026-05-24 20:39:59 +0300

    Add files via upload

---
+++ students_data.js
@@ -0,0 +1,67 @@
+// your code goes here
+// Общий файл с данными студентов
+// Изменения здесь автоматически отобразятся на всех страницах,
+// которые подключают этот файл (index_data.html, index_point.html и др.)
+
+window.studentsData = [
+    {
+        lastName: 'Иванов',
+        firstName: 'Артём',
+        middleName: 'Дмитриевич',
+        grade: 5
+    },
+    {
+        lastName: 'Петрова',
+        firstName: 'Анна',
+        middleName: 'Сергеевна',
+        grade: 4.5
+    },
+    {
+        lastName: 'Смирнов',
+        firstName: 'Максим',
+        middleName: 'Александрович',
+        grade: 3.8
+    },
+    {
+        lastName: 'Козлова',
+        firstName: 'Дарья',
+        middleName: 'Владимировна',
+        grade: 5
+    },
+    {
+        lastName: 'Новиков',
+        firstName: 'Егор',
+        middleName: 'Павлович',
+        grade: 4.2
+    },
+    {
+        lastName: 'Морозова',
+        firstName: 'София',
+        middleName: 'Алексеевна',
+        grade: 5
+    },
+    {
+        lastName: 'Волков',
+        firstName: 'Никита',
+        middleName: 'Романович',
+        grade: 2.5
+    },
+    {
+        lastName: 'Зайцева',
+        firstName: 'Мария',
+        middleName: 'Игоревна',
+        grade: 4.7
+    },
+    {
+        lastName: 'Соловьёв',
+        firstName: 'Денис',
+        middleName: 'Олегович',
+        grade: 3.3
+    },
+    {
+        lastName: 'Белова',
+        firstName: 'Алиса',
+        middleName: 'Андреевна',
+        grade: 5
+    }
+];
